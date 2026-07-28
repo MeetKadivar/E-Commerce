156ce400c7f945eed3084a4fcb32851889c188e9
Meet Kadivar <meetkadivar88@gmail.com>
2026-07-28 11:08:50 +0530

    Change routing

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,9 +4,9 @@ import './Navbar.css'
 function Navbar() {
   return (
     <nav className='nav-container'>
-      <NavLink to={`/`}>Home</NavLink>
-      <NavLink to={`/products`}>Products</NavLink>
-      <NavLink to={`cart`}>Cart</NavLink>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/products">Products</NavLink>
+      <NavLink to="/cart">Cart</NavLink>
     </nav>
   )
 }
